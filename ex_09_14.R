f019ceb15c0b5971c5f593190a01366ae2ba7893
Colin Rundel <rundel@gmail.com>
2016-09-14 17:50:30 -0400

    Ex2

--- ex_09_14.R
+++ ex_09_14.R
@@ -31,4 +31,37 @@ d = data.frame(
 
 ### Exercise 2
 
+primes = c(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 
+           43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+
+x = c(56, 3, 17, 2, 4, 9, 6, 5, 19, 5, 2, 3, 5, 0, 13, 12, 6, 31, 10, 21, 
+      8, 4, 1, 1, 2, 5, 16, 1, 3, 8, 1, 3, 4, 8, 5, 2, 8, 6, 18, 40, 10, 
+      20, 1, 27, 2, 11, 14, 5, 7, 0, 3, 0, 7, 0, 8, 10, 10, 12, 8, 82,
+      21, 3, 34, 55, 18, 2, 9, 29, 1, 4, 7, 14, 7, 1, 2, 7, 4, 74, 5, 
+      0, 3, 13, 2, 8, 1, 6, 13, 7, 1, 10, 5, 2, 4, 4, 14, 15, 4, 17, 1, 9)
+
+# Select every third value starting at position 2 in x. 
+
+x[seq(2, length(x), 3)]
+
+tmp=x[1:length(x)*3-1]
+tmp[!is.na(tmp)]
+
+# Remove all values with an odd index (e.g. 1, 3, etc.)
+
+x[ -((1:50)*2-1) ]
+x[-seq(1, length(x), 2)]
+
+# Select only the values that are primes. (You may assume all values are less than 100)
+
+x[x %in% primes]
+
+# Remove every 4th value, but only if it is odd.
+
+x[ !( (x %% 2 == 1) & seq_along(x) %in% seq(4,length(x),4) ) ]
+x[ !( (x %% 2 == 1) & c(FALSE,FALSE,FALSE,TRUE) ) ]
+
+x[ -intersect(which(x %% 2 == 1), seq(4,length(x),4))]
+
+
 ### Exercise 3
